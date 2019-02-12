772a605884b05efb2a2ae9e20df96d87b9e0e350
Guy <glando1991@gmail.com>
2019-02-12 13:53:04 +0200

    created the code

---
+++ MVC302RedirectCacheBug/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MVC302RedirectCacheBug.Models;
+
+namespace MVC302RedirectCacheBug.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            if (HttpContext.Request.Cookies.ContainsKey("ccc"))
+            {
+                return this.RedirectToAction("B", "Home");
+            }
+            else
+            {
+                return this.RedirectToAction("A", "Home");
+            }
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult A()
+        {
+            if (HttpContext.Request.Cookies.ContainsKey("ccc"))
+            {
+                return this.RedirectToAction("Index", "Home");
+            }
+
+            return View();
+        }
+
+        public IActionResult B()
+        {
+            return View();
+        }
+    }
+}
---
+++ MVC302RedirectCacheBug/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace MVC302RedirectCacheBug
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var config = new ConfigurationBuilder()
+                .AddCommandLine(args)
+                .SetBasePath(System.AppContext.BaseDirectory)
+                .AddEnvironmentVariables(prefix: "ASPNETCORE_")
+                .Build();
+            var application = new WebHostBuilder()
+                .UseConfiguration(config)
+                .UseIISIntegration()
+                .UseHttpSys()
+                .UseUrls("http://localhost:5000")
+                .UseStartup<Startup>()
+                .Build();
+            application.Run();
+        }
+    }
+}
